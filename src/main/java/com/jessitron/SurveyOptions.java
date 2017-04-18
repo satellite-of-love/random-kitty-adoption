9978a61d7cc0f0e5638baf1679d9e74264ec52a6
Jessica Kerr <jessitron@gmail.com>
2017-04-18 10:46:09 -0400

    implement

--- src/main/java/com/jessitron/SurveyOptions.java
+++ src/main/java/com/jessitron/SurveyOptions.java
@@ -1,19 +1,41 @@
 package com.jessitron;
 
-public class SurveyOptions {
-    
-    private int seed;
+import java.util.List;
 
-    public SurveyOptions(int seed) {
+public class SurveyOptions {
+
+    private String surveyName;
+    private int seed;
+    private List<SurveyOption> options;
+
+    public SurveyOptions(String surveyName, int seed, List<SurveyOption> options) {
+        this.surveyName = surveyName;
         this.seed = seed;
+        this.options = options;
     }
 
-    public int getSeed () {
+    public int getSeed() {
         return seed;
     }
 
-    public void setSeed (int seed) {
+    public void setSeed(int seed) {
         this.seed = seed;
+    }
+
+    public List<SurveyOption> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<SurveyOption> options) {
+        this.options = options;
+    }
+
+    public String getSurveyName () {
+        return surveyName;
+    }
+
+    public void setSurveyName (String surveyName) {
+        this.surveyName = surveyName;
     }
 
     // don't forget the default constructor. Jackson likes it
--- src/main/java/com/jessitron/SurveyOptionsController.java
+++ src/main/java/com/jessitron/SurveyOptionsController.java
@@ -5,13 +5,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @RestController
 public class SurveyOptionsController {
 
     @CrossOrigin()
     @RequestMapping(path = "/surveyOptions")
-    public SurveyOptions surveyOptions(@RequestParam(value = "seed") int seed) {
-        return new SurveyOptions(seed);
+    public SurveyOptions surveyOptions(@RequestParam(value = "seed") int seed)
+    {
+        return new SurveyOptions("Survey " + seed, seed, threeRandomOptions(seed));
+    }
+
+
+    private List<SurveyOption> threeRandomOptions(int seed) {
+        List<SurveyOption> local = new ArrayList<>(allOptions);
+        Collections.shuffle(local, new Random(seed)); // this actually modifies the list ugh
+        List<SurveyOption> withIndices = IntStream.range(0, local.size())
+                .mapToObj(i -> local.get(i).withPlace(i + 1))
+                .limit(3)
+                .collect(Collectors.toList());
+
+        return withIndices;
+    }
+
+
+    private final List<SurveyOption> allOptions;
+
+    { /* static initializer */
+        allOptions = new LinkedList<>();
+        allOptions.add(
+                new SurveyOption(
+                        "https://upload.wikimedia.org/wikipedia/commons/1/16/Kitty_meowing.jpg",
+                        "kitty meowing"));
+        allOptions.add(
+                new SurveyOption(
+                        "https://c1.staticflickr.com/4/3149/2988746750_4a3dfdee59.jpg",
+                        "sink kitties"));
+        allOptions.add(
+                new SurveyOption(
+                        "http://www.publicdomainpictures.net/pictures/60000/velka/green-eyed-kitty.jpg",
+                        "green-eyed kitty"));
+        allOptions.add(
+                new SurveyOption(
+                        "http://maxpixel.freegreatpicture.com/static/photo/1x/Sweet-Animals-Kitty-Cat-323262.jpg",
+                        "kitty licking paw"));
+        allOptions.add(
+                new SurveyOption(
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Computer-kitten.jpg/1024px-Computer-kitten.jpg",
+                        "computer kitten"));
     }
 
 }
